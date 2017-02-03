cdbb464467af9b1ec6495535dcd25a5cbb48f874
Patrick Luk <Patrick Luk>
2017-02-03 13:21:50 -0500

    testing

--- scripts/image-app-mine.js
+++ scripts/image-app-mine.js
@@ -1,12 +1,11 @@
-(function() {
+(function(){
   // http://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
   var original;
   var imageLoader = document.querySelector('#imageLoader');
   imageLoader.addEventListener('change', handleImage, false);
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
-
-  var imageWorker = new Worker('scripts/worker.js');
+  var imageWorker = new Worker("scripts/worker.js");
 
   function handleImage(e){
     var reader = new FileReader();
@@ -40,22 +39,42 @@
     var a, b, g, i, imageData, j, length, pixel, r, ref;
     imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
     toggleButtonsAbledness();
-    imageWorker.postMessage({'imageData': imageData, 'type': type});
 
-    imageWorker.onmessage = function(e) {
+    // Hint! This is where you should post messages to the web worker and
+    // receive messages from the web worker.
+    imageWorker.postMessage({'imageData':imageData, 'type':type});
+
+    imageWorker.onmessage = function(e){
       toggleButtonsAbledness();
       var image = e.data;
-      if (image) return ctx.putImageData(e.data, 0, 0);
-      console.log("No manipulated image returned.")
+      if(image) {
+        return ctx.putImageData(e.data, 0, 0);
+      }
+      console.log("No maniuplated image returned");
     }
 
-    imageWorker.onerror = function(error) {
-      function WorkerException(message) {
+    imageWorker.onerror = function(error){
+      function WorkerException(message){
         this.name = "WorkerException";
         this.message = message;
       };
-      throw new WorkerException('Worker error.');
+      throw new WorkerException("Worker error.");
     };
+
+    length = imageData.data.length / 4;
+    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
+      r = imageData.data[i * 4 + 0];
+      g = imageData.data[i * 4 + 1];
+      b = imageData.data[i * 4 + 2];
+      a = imageData.data[i * 4 + 3];
+      pixel = manipulate(type, r, g, b, a);
+      imageData.data[i * 4 + 0] = pixel[0];
+      imageData.data[i * 4 + 1] = pixel[1];
+      imageData.data[i * 4 + 2] = pixel[2];
+      imageData.data[i * 4 + 3] = pixel[3];
+    }
+    toggleButtonsAbledness();
+    return ctx.putImageData(imageData, 0, 0);
   };
 
   function revertImage() {
